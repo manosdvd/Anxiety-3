4a2a6fddf34c77448ec167e83f1cf8d2c78ace92
manosdvd <155906803+manosdvd@users.noreply.github.com>
2025-11-20 20:25:08 -0700

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        anxiety: resolve(__dirname, 'anxiety.html'),
+        dyslexia: resolve(__dirname, 'dyslexia.html'),
+        discalculia: resolve(__dirname, 'discalculia.html'),
+      },
+    },
+  },
+})
